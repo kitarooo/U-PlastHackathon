11149d01413e5a2845a5a4efe3e00f1e2e683ec8
azamatshauenbekov <bravlstarstop05@mail.ru>
2024-03-09 21:48:56 +0500

    Initial commit

--- src/main/java/backend/course/spring/uplasthackathon/UPlastHackathonApplication.java
+++ src/main/java/backend/course/spring/uplasthackathon/UPlastHackathonApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UPlastHackathonApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UPlastHackathonApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UPlastHackathonApplication.class, args);
+    }
 
 }
